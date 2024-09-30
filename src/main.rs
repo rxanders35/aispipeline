56e6db30bba494ad3b870e41a6099b00a2c4db04
rxanders35 <reidxanders@outlook.com>
2024-09-29 20:26:11 -0400

    download zip prototype

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,23 @@ use std::io::{Cursor, Write};
 use std::path::Path;
 use std::sync::Arc;
 use zip::read::ZipArchive;
+use std::error::Error;
 
 #[tokio::main]
 async fn main() {
 
 }
 
+async fn download_zip(url: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+    let response = reqwest::get(url).await?;
+    let bytes = response.bytes().await?;
+    Ok(bytes.to_vec())
+}
+
+async fn unpack_zip(){
+
+}
+
+async fn unpack_zip(){
+    todo!();
+}
