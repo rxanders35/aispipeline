e804e1d67df64b2cd192d192a5c86cb9c9cea8b8
rxanders35 <reidxanders@outlook.com>
2024-09-29 21:17:34 -0400

    unpack basic idea

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use arrow::csv::ReaderBuilder;
 use arrow::record_batch::RecordBatch;
 use parquet::arrow::ArrowWriter;
 use reqwest;
+use core::arch;
+use std::fs;
 use std::fs::File;
+use std::fs::create_dir;
 use std::io::{Cursor, Write};
 use std::path::Path;
 use std::sync::Arc;
@@ -10,8 +13,11 @@ use zip::read::ZipArchive;
 use std::error::Error;
 
 #[tokio::main]
-async fn main() {
+async fn main() { 
+    let url = "http://web.ais.dk/aisdata/aisdk-2024-09-26.zip";
+    let zip_data = download_zip(url).await.unwrap();
 
+    
 }
 
 async fn download_zip(url: &str) -> Result<Vec<u8>, Box<dyn Error>> {
@@ -20,10 +26,15 @@ async fn download_zip(url: &str) -> Result<Vec<u8>, Box<dyn Error>> {
     Ok(bytes.to_vec())
 }
 
-async fn unpack_zip(){
-
-}
-
-async fn unpack_zip(){
-    todo!();
-}
+fn unpack_zip(zip_data: Vec<u8>) -> Result<Vec<String>, Box<dyn Error>> {
+    let path = "/medallion/bronze"; 
+    
+    let mut archive = ZipArchive::new(Cursor::new(zip_data))?;
+    let csv_files = Vec::new();
+    
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        if file.name().ends_with(".csv") {
+            csv_files.push(path.clone());
+        }
+    }
