61a586b42ce85e12ba6077e8c0a74251617c1525
rxanders35 <reidxanders@outlook.com>
2024-09-29 22:21:28 -0400

    reorganize process

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,12 @@ use std::error::Error;
 #[tokio::main]
 async fn main() { 
     let url = "http://web.ais.dk/aisdata/aisdk-2024-09-26.zip";
-    let zip_data = download_zip(url).await.unwrap();
+    let bronze_zone = "/medallion/bronze";
+    let silver_zone = "/medallion/silver";
 
+    //let zip_data = download_zip(url).await.unwrap();
+    //let zip_file_path = format("{}/aisdk-2024-09-26.zip", bronze_zone);
+    //let csv_files = unpack_zip(zip_data, bronze_zone)?;
     
 }
 
@@ -26,16 +30,13 @@ async fn download_zip(url: &str) -> Result<Vec<u8>, Box<dyn Error>> {
     Ok(bytes.to_vec())
 }
 
-fn unpack_zip(zip_data: Vec<u8>) -> Result<Vec<String>, Box<dyn Error>> {
-    let path = "/medallion/bronze"; 
-    
-    let mut archive = ZipArchive::new(Cursor::new(zip_data))?;
-    let csv_files = Vec::new();
-    
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i)?;
-        if file.name().ends_with(".csv") {
-            csv_files.push(path.clone());
-        }
-    }
+fn zip_to_bronze(zip_file_path: &str, zip_data: &[u8]) -> Result<(), Box<dyn Error>> {
+    let mut file = File::create(zip_file_path)?;
+    file.write_all(zip_data)?;
+    Ok(())
+}
+
+
+fn unpack_zip(zip_data: Vec<u8>, bronze_zone: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    todo!("later")
 }
