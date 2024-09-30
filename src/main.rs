b945a69f5d9388eb59c51c26153a452c9b51a9c8
rxanders35 <reidxanders@outlook.com>
2024-09-29 20:03:20 -0400

    use relevant crate modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use arrow::csv::ReaderBuilder;
+use arrow::record_batch::RecordBatch;
+use parquet::arrow::ArrowWriter;
+use reqwest;
+use std::fs::File;
+use std::io::{Cursor, Write};
+use std::path::Path;
+use std::sync::Arc;
+use zip::read::ZipArchive;
+
+#[tokio::main]
+async fn main() {
+
 }
+
