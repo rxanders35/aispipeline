b929aaa14130d80adbdb1afd6c51ec5b7d7cdfa9
rxanders35 <reidxanders@outlook.com>
2025-04-12 11:06:26 -0400

    parse html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, World!");
+use anyhow::Result;
+use scraper::Html;
+
+struct PageData;
+
+async fn process_html(url: &str) -> Result<Html> {
+    let body = reqwest::get(url).await?.text().await?;
+    let html = Html::parse_document(&body);
+    return Ok(html);
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url = "https://web.ais.dk/aisdata/";
+    let html = process_html(url).await?;
+    Ok(())
 }
