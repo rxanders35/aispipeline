38b63279e1a485ad3af70f82200ac4766382b0db
rxanders35 <reidxanders@outlook.com>
2025-04-12 11:49:27 -0400

    client

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 use anyhow::Result;
-use scraper::Html;
+use reqwest::Client;
+use scraper::{Html, Selector};
 
 struct PageData;
 
-async fn process_html(url: &str) -> Result<Html> {
-    let body = reqwest::get(url).await?.text().await?;
+async fn process_html(body: String) -> Result<Html> {
     let html = Html::parse_document(&body);
+    let row_selector = Selector::parse("tr").unwrap();
+    let link_selector = Selector::parse("a[href]").unwrap();
+    let td_selector = Selector::parse("td[align=\"right\"]").unwrap();
+
+    let mut res: Vec<_> = Vec::new();
+
+    for row in html.select(&row_selector) {
+        let link = row
+            .select(&row_selector)
+            .next()
+            .and_then(|elem| elem.value().attr("href"))
+            .filter(|href| href.ends_with(".zip"));
+    }
+
     return Ok(html);
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let url = "https://web.ais.dk/aisdata/";
-    let html = process_html(url).await?;
+    let client = Client::new();
+    let response = client.get(url).send().await;
+    let body = response.unwrap().text().await.unwrap();
+    let html = process_html(body);
     Ok(())
 }
